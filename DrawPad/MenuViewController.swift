e627ed88367b66698123454bff179a129ecaf87a
steevenelvis <a01370440@DPDMASTER-101.local>
2015-10-12 18:47:49 -0500

    Primera

---
+++ DrawPad/MenuViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  proyecto
+//
+//  Created by Steeven Elvis Muñoz Pincay on 14/09/15.
+//  Copyright (c) 2015 Steeven Elvis Muñoz Pincay. All rights reserved.
+//
+
+import UIKit
+
+
+class MenuViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "fondo.jpg")!)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
